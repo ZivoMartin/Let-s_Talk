f799444a35e34201726a2de41b9a3db77d0eb5dd
Martin Zivojinovic <martin.zivojinovic@etu.u-bordeaux.fr>
2023-11-04 23:12:36 +0100

    KAKATEKOI BICH

--- chat.py
+++ chat.py
@@ -2,6 +2,7 @@
 import socket
 import sys
 import select
+from random import randint
 
 s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -9,10 +10,14 @@ port = 7777
 s.bind(("", port))
 s.listen(1)
 clients = {}
-channels = {"Global": {"membres": [], "admin": None}}
+id_tab = [None]*10001
+id_tab[0] = "Global"
+channels = {"Global": {"membres": [], "admin": None, "id": 0}}
 running = True
 admin = ""
 nb_user = 1
+wall = "ceciestunwall__||$!/\!$||__ceciestunwall"
+
 while(running):
     sockets = []
     for client in clients.values():
@@ -24,118 +29,86 @@ while(running):
             new_s, adr = s.accept()
             clients[f"user{nb_user}"] = {"socket": new_s, "current_channel": ["Global"]}
             channels["Global"]["membres"].append(f"user{nb_user}")
+            new_s.sendall(f"CONNECT/|/Système/|/user{nb_user}{wall}".encode())
+            new_s.sendall(f"MSG/Global/Système/|/Bienvenue sur le chanel Global, ici tout le monde peut lire vos messages. Tâchez d'étre respéctueux.{wall}".encode())
             nb_user += 1
-            print("connected !")
         else:
             data = l1[i].recv(1500).decode()
-            if(data != "\n"):
-                sender = ""
-                for client in clients.keys():
-                    if(clients[client]["socket"] == l1[i]):
-                        sender = client
-                        break       
-                splited_data = data.split(" ") 
-                cmd = splited_data[0].split("\n")[0]
+            splited_data = data.split("/") 
+            cmd = splited_data[0]
+            channel = splited_data[1]
+            sender = splited_data[2]
+            receiver = splited_data[3]
+            for j in range(4):
                 splited_data.pop(0)
-                channel = ""
-                msg = ""
-                if(len(splited_data) > 0):
-                    channel = splited_data[0].split("\n")[0]
-                    splited_data.pop(0)
+            content = "".join(splited_data)
+
+            if(cmd == "MSG"):
+                   
+                for client in channels[channel]["membres"]:
+                    if(client != sender):
+                        clients[client]["socket"].sendall((f"MSG/{channel}/{sender}/|/{content}{wall}").encode())
                     
-                if(len(splited_data) > 0):
-                    nb_word = len(splited_data)
-                    for j in range(nb_word):
-                        msg += splited_data[j]+" "
-                msg = msg.split("\n")[0]
-                if(cmd == "MSG"):
-                    if(channel != "" or msg != ""):
-                        if(channel not in clients[sender]["current_channel"]):
-                            l1[i].sendall((f"Vous n'avez pas encore rejoint le channel {channel} !\n").encode())
-                        else:
-                            for client in channels[channel]["membres"]:
-                                if(clients[client]["socket"] != l1[i]):
-                                    clients[client]["socket"].sendall((f"{channel}~ [{sender}] : {msg}\n").encode())
-                    elif(channel == ""):
-                        l1[i].sendall("Vous devez preciser un channel\n".encode())
-                        
-                elif(cmd == "NICK"):
-                    if(channel == ""):
-                        l1[i].sendall("Le nom choisis n'est pas correct.\n".encode())
-                    elif(channel in clients.keys()):
-                        l1[i].sendall("Ce nom est déjà utilisé\n".encode())
-                    else:
-                        clients[channel] = {"socket": clients[sender]["socket"], "current_channel": clients[sender]["current_channel"]}
-                        del clients[sender]
-                        for elt in channels.keys():
-                            if(sender in channels[elt]["membres"]):
-                                channels[elt]["membres"].remove(sender)
-                                channels[elt]["membres"].append(channel)
-                    
-                elif(cmd == "NAMES"):
-                    if(channel not in clients[sender]["current_channel"]):
-                        l1[i].sendall((f"Vous n'avez pas encore rejoint le channel {channel} !\n").encode())
-                    else:
-                        response = "Clients: "
-                        for usr in channels[channel]["membres"]:
-                            response += usr + " "
-                        l1[i].sendall((response+"\n").encode())
-                        
-                elif(cmd == "BYEBYE"):
-                    l1[i].close()
-                    del clients[sender]
-                    
-                elif(cmd == "REMOVE"):
-                    if(channel not in clients[sender]["current_channel"]):
-                        l1[i].sendall((f"Vous n'avez pas encore rejoint le channel {channel} !\n").encode())
-                    elif(len(splited_data) == 0):
-                        l1[i].sendall((f"Vous n'avez pas indiqué l'utilisateur à exclure !\n").encode())
-                    else:
-                        kiked_user = splited_data[0]
-                        splited_data.pop(0)
-                        msg = ""
-                        if(len(splited_data) > 0):
-                            nb_word = len(splited_data)
-                            for j in range(nb_word):
-                                msg += splited_data[j]+" "
-                        if(l1[i] == channels[channel]["admin"] and kiked_user in channels[channel]["membres"]):
-                            clients[kiked_user]["socket"].sendall(f"Tu as été exclu du channel {channel} (force à toi): {msg}\n".encode())
-                            clients[kiked_user]["current_channel"].remove(channel)
-                            channels[channel]["membres"].remove(kiked_user)
-                        else:
-                            if(l1[i] != channels[channel]["admin"]):
-                                l1[i].sendall("Tu dois étre l'admin du channel, bon courage pour ton coup d'état.\n".encode())
-                            else:
-                                l1[i].sendall("Cet utilisateur n'est pas sur ce channel\n".encode())
-                                
-                elif(cmd == "JOIN"):
-                    if(channel in clients[sender]["current_channel"]):
-                       l1[i].sendall((f"Vous étes déjà dans ce channel !\n").encode())
-                    else:
-                        if(channel not in channels.keys()):
-                            l1[i].sendall((f"Vous venez de créer le channel {channel} !\n").encode())
-                            channels[channel] = {"admin": l1[i], "membres": [sender]}
-                            clients[sender]["current_channel"].append(channel)
-                        else:
-                            for usr in channels[channel]["membres"]:
-                                clients[usr]["socket"].sendall((f"{sender} a rejoint le channel {channel} !\n").encode())
-                            l1[i].sendall((f"Vous avez rejoins le channel {channel} !\n").encode())
-                            channels[channel]["membres"].append(sender)
-                            clients[sender]["current_channel"].append(channel)
-                           
-                
-                elif(cmd == "PART"):
-                    if(channel not in clients[sender]["current_channel"]):
-                        l1[i].sendall((f"Vous n'étes pas sur le channel {channel} !\n").encode())
-                    else:
-                        l1[i].sendall((f"Vous avez quitté le channel {channel} !\n").encode())
-                        channels[channel]["membres"].remove(sender)
-                        clients[sender]["current_channel"].remove(channel)
-                        for usr in channels[channel]["membres"]:
-                            clients[usr]["socket"].sendall((f"{usr} a quitté le channel {channel} : {msg}\n").encode())
-                
+            elif(cmd == "NICK"):
+                if(content in clients.keys()):
+                    l1[i].sendall(f"Ce nom est déjà utilisé{wall}".encode())
                 else:
-                    l1[i].sendall(f"Commande invalide: {cmd}\n".encode())
+                    clients[content] = {"socket": clients[sender]["socket"], "current_channel": clients[sender]["current_channel"]}
+                    del clients[sender]
+                    for elt in channels.keys():
+                        if(sender in channels[elt]["membres"]):
+                            channels[elt]["membres"].remove(sender)
+                            channels[elt]["membres"].append(content)
+                
+            elif(cmd == "NAMES"):
+                response = ""
+                for usr in channels[channel]["membres"]:
+                    response += usr + "|"
+                l1[i].sendall((response+wall).encode())
+                    
+            elif(cmd == "BYEBYE"):
+                l1[i].close()
+                del clients[sender]
+                for channel in channels.values():
+                    if(sender in channel["membres"]):
+                        channel["membres"].remove(sender)
+                        for usr in channel["membres"]:
+                            clients[usr]["socket"].sendall(f"MSG/{channel}/Système/|/{sender} à désactivé son compte.{wall}".encode())
+                
+            elif(cmd == "REMOVE"):
+
+                clients[receiver]["socket"].sendall(f"REMOVED/{channel}/{sender}/|/|{wall}".encode())
+                clients[receiver]["current_channel"].remove(channel)
+                channels[channel]["membres"].remove(receiver)
+                for usr in channels[channel]["membres"]:
+                    clients[usr]["socket"].sendall((f"MSG/{channel}/système/|/{receiver} a été exclu par {sender}.{wall}").encode())
+                            
+            elif(cmd == "JOIN"):
+                content = int(content)
+                if(content < len(id_tab) and id_tab[content] != None and id_tab[content] not in clients[sender]["current_channel"]):
+                    for usr in channels[id_tab[content]]["membres"]:
+                        clients[usr]["socket"].sendall((f"MSG/{id_tab[content]}/Système/|/{sender} a rejoint le channel, accueilez le comme il se doit.{wall}").encode())
+                    l1[i].sendall((f"JOIN/|/Système/|/{id_tab[content]}{wall}").encode())
+                    l1[i].sendall((f"MSG/{id_tab[content]}/Système/|/Vous avez rejoins le channel {id_tab[content]} !{wall}").encode())
+                    channels[id_tab[content]]["membres"].append(sender)
+                    clients[sender]["current_channel"].append(id_tab[content])
+            
+            elif(cmd == "CREATE"):
+                channel_id = randint(1, 10000)
+                while(channel_id in id_tab):
+                    channel_id = randint(1, 10000)
+                id_tab[channel_id] = content
+                l1[i].sendall((f"MSG/{content}/Système/|/Vous venez de créer le channel {content}, son mot de passe est {channel_id}, utilisez le pour inviter vos amis.{wall}").encode())
+                channels[content] = {"admin": l1[i], "membres": [sender], "id": channel_id}
+                clients[sender]["current_channel"].append(content)
+            
+            elif(cmd == "PART"):
+                channels[channel]["membres"].remove(sender)
+                clients[sender]["current_channel"].remove(channel)
+                for usr in channels[channel]["membres"]:
+                    clients[usr]["socket"].sendall((f"MSG/{channel}/système/|/{sender} est parti.{wall}").encode())
+            
+
 s.close()
 
 
--- client.py
+++ client.py
@@ -4,104 +4,165 @@ import tkinter as tk
 import threading
 
 
+
 def listen():
     global data
-    while data == "":
-        data = s.recv(1024).decode()
+    while True:
+        recu = s.recv(1024).decode()
+        split_recu = recu.split(wall)
+        nb_request = len(split_recu)
+        for i in range(nb_request-1):
+            data.append(split_recu[i])
+
 
 def start_thread():
     thread = threading.Thread(target=listen)
     thread.start()
 
 def event_gestion(): 
-    global data   
-    if(data != ""):
-        data = ""
-        start_thread()
-    window.after(5, event_gestion)
+    global data  
+    global name 
+    for request in data:
+        splited_data = request.split("/")
+        print(request)
+        data_type = splited_data[0]
+        channel = splited_data[1]
+        sender = splited_data[2]
+        receiver = splited_data[3]
+        for i in range(4):
+            splited_data.pop(0)
+        content = "".join(splited_data)
+        if(data_type == "MSG"):
+            new_message(content, channel, sender) 
+        elif(data_type == "CONNECT"):
+            name = content
+        elif(data_type == "JOIN"):
+            config_a_channel_button(content)
+            switch_window(frames["join_frame"], frames["main_frame"])
+
+    data = []
+    window.after(1, event_gestion)
 
 def config_window():
     window.geometry("1500x800")
     window.title("Let's Talk")
     window.config(bg="dark blue")
-    config_a_button("Global")
-    display_main_menu()
+    new_channel_button.grid(row=1000, column=0)
+    join_channel_button.grid(row=1000, column=1, sticky="se")
+    new_channel_input.grid(row=1, column=0)
+    join_channel_input.grid(row=1, column=0)
+    back_button_channel.grid(row=0, column=0)
+    back_button_create.grid(row=0, column=0)
+    back_button_join.grid(row=0, column=0)
+    current_label_conv.grid(row=1, column=0)
+    send_message_input.grid(row=2, column=0)
+    config_a_channel_button("Global")
+    window.protocol("WM_DELETE_WINDOW", close_window)
+    frames["main_frame"].grid(row=0, column=0)
 
 
-def config_a_button(channel):
-    channels[channel] = {"button": tk.Button(window), "messages": ["[Système]: Bienvenue sur le channel "+channel+" ! Merci d'ètre polie et courtoit.\n"]}
+def config_a_channel_button(channel):
+    channels[channel] = {"button": tk.Button(frames["main_frame"]), "messages": []}
     channels[channel]["button"].config(text=channel, bg="black", fg="white", command=lambda c=channel: go_on_a_channel(c))
+    channels[channel]["button"].grid(row=len(channels), column=0, columnspan=2, sticky="ew")
 
-def display_main_menu():
-    for channel in channels.values():
-        channel["button"].pack(side="top", fill="x")
-    new_channel_button.pack(side="bottom")
-
-def hide_main_menu():
-    for channel in channels.values():
-        channel["button"].pack_forget()
-    new_channel_button.pack_forget()
 
 def go_on_a_channel(c):
     global current_channel
-    hide_main_menu()
+    switch_window(frames["main_frame"], frames["channel_frame"])
     current_channel = c
     txt = ""
     for message in channels[c]["messages"]:
         txt += message
     current_label_conv.config(text=txt)
-    channel_frame.grid(row=1, column=0)
-    back_button.grid(row=0, column=0)
-    current_label_conv.grid(row=1, column=0)
-    send_message_input.grid(row=2, column=0)
 
-def go_back():
-    channel_frame.grid_forget()
-    join_frame.grid_forget()
-    back_button.grid_forget()
-    display_main_menu()
 
-def join_button():
-    hide_main_menu()
-    back_button.grid(row=0, column=0)
-    join_frame.grid(row=1, column=0)
-    new_channel_input.grid(row=1, column=0)
 
-def join_this_channel(event):
+def switch_window(previous_frame, new_frame):
+    previous_frame.grid_forget()
+    new_frame.grid(row=0, column=0)
+
+
+
+def create_new_channel(event):
     new_channel = new_channel_input.get()
-    config_a_button(new_channel)
-    go_back()
+    new_channel_input.delete(0, 'end')
+    config_a_channel_button(new_channel)
+    s.sendall(f"CREATE/|/{name}/|/{new_channel}".encode())
+    switch_window(frames["create_frame"], frames["main_frame"])
+    
+def join_new_channel(event):
+    code = join_channel_input.get()
+    join_channel_input.delete(0, 'end')
+    correct = True
+    chiffres = "1234567890"
+    for elt in code:
+        if(elt not in chiffres):
+            correct = False
+            break
+    if(correct):
+        s.sendall(f"JOIN/|/{name}/|/{code}".encode())
+    
 
 def send_message(event):
-    msg = "[Vous]: "+send_message_input.get()+"\n"
-    channels[current_channel]["messages"].append(msg)
-    txt = ""
-    for message in channels[current_channel]["messages"]:
-        txt += message
-    current_label_conv.config(text=txt)
+    msg = send_message_input.get()
     send_message_input.delete(0, 'end')
+    new_message(msg, current_channel, name)
+    s.sendall(f"MSG/{current_channel}/{name}/|/{msg}".encode())
+
+def new_message(msg, channel, user):
+    channels[channel]["messages"].append(f"[{user}]: {msg}\n")
+    if(channel == current_channel):
+        txt = ""
+        for message in channels[current_channel]["messages"]:
+            txt += message
+        current_label_conv.config(text=txt)
+
+def close_window():
+    s.sendall(f"BYEBYE/|/{name}/|/|".encode())
+    window.destroy()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "localhost"
 port = 7777
-
-#s.connect((host, port)) 
-data = ""
+name = ""
+wall = "ceciestunwall__||$!/\!$||__ceciestunwall"
+s.connect((host, port)) 
+data = []
 window = tk.Tk()
 channels = {}
 current_channel = None
-join_frame = tk.Frame(window, bg="dark blue")
-channel_frame = tk.Frame(window, bg="dark blue")
-current_label_conv = tk.Label(channel_frame, bg="black", fg="white")
-back_button = tk.Button(window, text="Back", bg="blue", fg="white", command=go_back)
-new_channel_button = tk.Button(window, text="Join/Create a channel ?", bg="blue", fg="white", command=join_button)
-new_channel_input = tk.Entry(join_frame, bg="blue", fg="white")
-new_channel_input.bind("<Return>", lambda e:join_this_channel(e))
-send_message_input = tk.Entry(channel_frame, bg="blue", fg="white")
+
+frames = {
+    "main_frame":tk.Frame(window, bg="dark blue"), 
+    "create_frame": tk.Frame(window, bg="dark blue"),
+    "channel_frame":  tk.Frame(window, bg="dark blue"),
+    "join_frame":  tk.Frame(window, bg="dark blue")
+}
+
+back_button_create = tk.Button(frames["create_frame"], text="Back", bg="blue", fg="white", command=lambda: switch_window(frames["create_frame"], frames["main_frame"]))
+back_button_join = tk.Button(frames["join_frame"], text="Back", bg="blue", fg="white", command=lambda: switch_window(frames["join_frame"], frames["main_frame"]))
+back_button_channel = tk.Button(frames["channel_frame"], text="Back", bg="blue", fg="white", command=lambda: switch_window(frames["channel_frame"], frames["main_frame"]))
+
+new_channel_button = tk.Button(frames["main_frame"], text="Create a new channel ?", bg="blue", fg="white", command=lambda: switch_window(frames["main_frame"], frames["create_frame"]))
+join_channel_button = tk.Button(frames["main_frame"], text="Join a new channel ?", bg="blue", fg="white", command=lambda: switch_window(frames["main_frame"], frames["join_frame"]))
+
+new_channel_input = tk.Entry(frames["create_frame"], bg="blue", fg="white")
+new_channel_input.bind("<Return>", lambda e:create_new_channel(e))
+
+join_channel_input = tk.Entry(frames["join_frame"], bg="blue", fg="white")
+join_channel_input.bind("<Return>", lambda e:join_new_channel(e))
+
+current_label_conv = tk.Label(frames["channel_frame"], bg="black", fg="white")
+
+send_message_input = tk.Entry(frames["channel_frame"], bg="blue", fg="white")
 send_message_input.bind("<Return>", lambda e:send_message(e))
 
+
 config_window()
-#start_thread()
-#event_gestion()
+
+
+start_thread()
+event_gestion()
 window.mainloop()
 
